c92b7338e9396dfe78e2029ca45308673b641f47
Rodrigo Munoz <Rodrigo.munozmo@sansano.usm.cl>
2025-11-08 12:10:46 -0300

    Limpiando campos sobrepuesto

--- dragino_lora_app/main.cpp
+++ dragino_lora_app/main.cpp
@@ -349,9 +349,9 @@ void sendToMQTT(char* payload) {
     
     MQTTClient_waitForCompletion(client, token, TIMEOUT); // Espera que la publicacion previa se complete, es decir, que el broker confirme la recepcion del mensaje (dependiendo del QoS)
     //fprintf(stderr, "Antes de enviar por sendToMQTT()\n");
-    printf("Message sent: %s\n", payloadWithRSSI);
+    //printf("Topico R: %s\n", payloadWithRSSI);
 
-    // Limpiar buffer para el próximo mensaje
+    // Limpiar buffer para el proximo mensaje
     payloadWithRSSI[0] = '\0';
     payload[0] = '\0';
 
